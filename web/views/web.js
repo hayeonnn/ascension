a67a6b622dff98324c44cd168d9ebce0279f06cb
hayeon <thsgkdus@thinkware.co.kr>
2023-01-18 17:46:05 +0900

    경로수정

--- web/views/web.js
+++ web/views/web.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const PORT = 8001
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/web/views');
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
   res.render('index')
