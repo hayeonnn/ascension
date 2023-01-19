831e8c39bbd64b81375a0f829aa8a1b68a2de7b0
hayeon <cygnets99@gmail.com>
2023-01-19 21:25:20 +0900

    test

--- web.js
+++ web.js
@@ -2,26 +2,12 @@ import express from "express";
 import mysql from "mysql";
 import cors from "cors";
 import path from "path";
+import "./assets/scss/style.scss";
+import Loader from "./layouts/loader/Loader";
+import App from "./App";
 
 const app = express();
 const port = process.env.PORT || 8002;
-app.use(express.static("uploads"));
-
-app.use(express.static(path.join(__dirname, "/client/build")));
-
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/client/build/index.html"));
-});
-
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "/client/build/index.html"));
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
-// mysql 모듈 사용
-//const mysql = require("mysql");
 
 // 연결할 DB 정보입력
 const connection = mysql.createConnection({
@@ -34,6 +20,28 @@ const connection = mysql.createConnection({
 
 // 데이터베이스 연결
 connection.connect();
+
+app.use(express.static("uploads"));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/src/views/index.ejs"));
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/src/views/index.ejs"));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
+/*
+
+      app.set("views", __dirname + "/src");
+      app.set("view engine", "ejs");
+      app.get("/", (req, res) => {
+        res.render("index");
+      });
+*/
 
 /**
 // create 쿼리문 사용
@@ -48,15 +56,9 @@ connection.query('describe books', (error, results, fields) => {
 });
 
 // 연결 종료
-connection.end(); */
-//앱 실행
+connection.end(); 
 
-app.set("views", __dirname + "/src");
-app.set("view engine", "ejs");
-app.get("/", (req, res) => {
-  res.render("index");
-});
-/*
+//앱 실행
 app.listen(PORT, () => {
   console.log(`server started on PORT ${PORT}`);
 });
