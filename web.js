f83111177577e7e4837fe35d606ea92c4a577dcb
hayeon <cygnets99@gmail.com>
2023-01-20 00:24:33 +0900

    tq

--- web.js
+++ web.js
@@ -33,11 +33,11 @@ connection.connect();
 app.use(express.static(path.join(__dirname, "/build")));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "ascension/build/index.html"));
+  res.sendFile(path.join(__dirname, "/build/index.html"));
 });
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "ascension/build/index.html"));
+  res.sendFile(path.join(__dirname, "/build/index.html"));
 });
 /**const React = request("react");
 const ReactDOM = request("react-dom");
