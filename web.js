6555bf22aeed54889d4a3ef15fb6d16bab8aba65
hayeon <thsgkdus@thinkware.co.kr>
2023-01-19 13:52:16 +0900

    mysql 연결

--- web.js
+++ web.js
@@ -10,3 +10,33 @@ app.listen(PORT, () => {
   console.log(`server started on PORT ${PORT}`)
 })
 // web.js
+
+// mysql 모듈 사용
+const mysql = require('mysql');
+
+// 연결할 DB 정보입력
+const connection = mysql.createConnection({
+  host: 'ascension.cafe24app.com',
+  user: 'ascension0',
+  password: 'shy66300!',
+  database: 'ascension0',
+  port: '3306',
+});
+
+// 데이터베이스 연결
+connection.connect();
+
+/**
+// create 쿼리문 사용
+connection.query('쿼리문', (error, results, fields) => {
+  if (error) throw error;
+  console.log(results);
+});
+
+connection.query('describe books', (error, results, fields) => {
+  if (error) throw error;
+  console.log(results);
+});
+
+// 연결 종료
+connection.end(); */
