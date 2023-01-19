516c6f3fa36385ce3aba18b6d41264b397719244
hayeon <thsgkdus@thinkware.co.kr>
2023-01-19 17:34:32 +0900

    remain

--- web.js
+++ web.js
@@ -1,17 +1,23 @@
-const express = require('express')
-const app = express()
-const PORT = 8001
+//import React, { Suspense } from "react";
+//import Loader from "./views/layouts/loader/Loader.js";
+//import { HashRouter } from "react-router-dom";
+//import App from "./views/App.js";
+import express from "express";
+import mysql from "mysql";
+//const express = require("express");
+const app = express();
+const PORT = 8001;
 
 // mysql 모듈 사용
-const mysql = require('mysql');
+//const mysql = require("mysql");
 
 // 연결할 DB 정보입력
 const connection = mysql.createConnection({
-  host: 'ascension.cafe24app.com',
-  user: 'ascension0',
-  password: 'shy66300!',
-  database: 'ascension0',
-  port: '3306',
+  host: "ascension.cafe24app.com",
+  user: "ascension0",
+  password: "shy66300!",
+  database: "ascension0",
+  port: "3306",
 });
 
 // 데이터베이스 연결
@@ -31,12 +37,14 @@ connection.query('describe books', (error, results, fields) => {
 
 // 연결 종료
 connection.end(); */
+//앱 실행
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs')
-app.get('/', (req, res) => {
-  res.render('index')
-})
+app.set("views", __dirname + "/src");
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
 app.listen(PORT, () => {
-  console.log(`server started on PORT ${PORT}`)
-})
+  console.log(`server started on PORT ${PORT}`);
+});
